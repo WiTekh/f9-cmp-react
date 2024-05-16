ee6ef355a44112023dfa7b1aabe62b415799df6d
juju <julien.delbosc@epita.fr>
2024-05-16 16:17:40 +0200

    add : terraform integration that will inevitably crash bc we suck at programming

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ButtonComponent from './Button';
 
-const { TC } = require('terraform-cloud');
-
 function onClick(){
   console.log("Terraform here")
 }
--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,24 @@
 import React from 'react';
+const { TerraformCloud } = require('terraform-cloud');
 
 class ButtonComponent extends React.Component {
   handleClick = () => {
     console.log("Terraform here");
+    // Configurer l'accès à Terraform Cloud
+    const terraformCloud = new TerraformCloud({
+        organization: 'your-organization',
+        workspace: 'your-workspace',
+        apiToken: 'your-api-token',
+    });
+  
+  // Appliquer les modifications
+  terraformCloud.apply('auto-approve')
+    .then((result) => {
+      console.log('Changements appliqués avec succès:', result);
+    })
+    .catch((error) => {
+      console.error('Erreur lors de l\'application des changements:', error);
+    });
   }
 
   render() {
